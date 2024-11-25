d8a9effb7c68834b227fbdc2f97ff84e378fc2ca
Billa Saikumar <BS00779804@techmahindra.com>
2024-11-25 17:02:24 +0530

    pushingTailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{ts,js,tsx,jsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        glow: {
-          "0%, 100%": { textShadow: "0 0 10px rgba(255, 255, 255, 0.8)" },
-          "50%": { textShadow: "0 0 20px rgba(255, 255, 255, 1)" },
-        },
-      },
-      animation: {
-        glow: "glow 2s infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
